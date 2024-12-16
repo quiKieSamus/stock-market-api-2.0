0996a2771d170eaf66d3e55cb1d809ca66405d62
Ruben Gonzalez <gonzru01atwork@gmail.com>
2024-12-16 11:07:45 -0400

    feat: empresas prices routes

--- src/controllers/EmpresaController.js
+++ src/controllers/EmpresaController.js
@@ -40,4 +40,45 @@ export class EmpresaController {
             return res.status(500).send(`Error in server ${err}`);
         }
     }
+    static async getPrecios(req, res) {
+        try {
+            const empresas = await getAllEmpresas();
+            const updates = await getAllUpdates();
+
+            const empresasWithUpdates = empresas.map((empresa) => {
+                return {
+                    empresa: empresa,
+                    updates: updates.filter((updates) => updates.idEmpresa == empresa.id)
+                }
+            });
+
+            const empresasWithLastPrices = empresasWithUpdates.map((empresasUpdate) => {
+                const updatesSorted = empresasUpdate.updates.sort((a, b) => new Date(a.dateHour) < new Date(b.dateHour));
+                const newestUpdate = updatesSorted[updatesSorted.length - 1];
+                const secondNewestUpdate = updatesSorted.filter((update) => {
+                    return !areDatesStringEqual(update.dateHour.replace(" ", "T"), newestUpdate.dateHour.replace(" ", "T"), true);
+                }).sort((a, b) => new Date(a.dateHour) - new Date(b.dateHour));
+
+                return {
+                    empresa: {
+                        ...empresasUpdate.empresa,
+                        startPrice: newestUpdate?.price ?? {},
+                        endPrice: secondNewestUpdate[secondNewestUpdate.length - 1]?.price ?? newestUpdate
+                    },
+                    updates: {
+                        updatesSorted: updatesSorted,
+                        closing: newestUpdate ?? {},
+                        beginning: secondNewestUpdate[secondNewestUpdate.length - 1] ?? newestUpdate
+                    }
+                }
+            });
+
+            if (!req.query.simbolo) {
+                return res.json(empresasWithLastPrices);
+            }
+            return res.json(empresasWithLastPrices.filter((empresaUpdate) => empresaUpdate.empresa.symbol == req.query.simbolo));
+        } catch (err) {
+            return res.status(500).send(`Error in server ${err}`);
+        }
+    }
 }
--- src/routes/EmpresaRouter.js
+++ src/routes/EmpresaRouter.js
@@ -5,3 +5,4 @@ import { EmpresaController } from "../controllers/EmpresaController.js";
 export const empresaRouter = Router();
 
 empresaRouter.get("/", (req, res) => EmpresaController.get(req, res));
+empresaRouter.get("/precios", (req, res) => EmpresaController.getPrecios(req, res))
